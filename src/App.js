540369b62f6ac46c04b4301d28acdd95a1c09e12
BuildTools <arthurpisakhov1@gmail.com>
2019-10-05 17:59:56 -0400

    pagination works

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchHSCards } from './actions/index';
 import PlayerHand from './components/PlayerHand';
 import CardsDisplay from './components/CardsDisplay';
 
 function App(props) {
-  const fetchCards = e => {
-    e.preventDefault();
+  useEffect(() => {
     props.fetchHSCards()
-  }
-
-  const testState = () => (
-    console.log(props.playerHand)
-  )
+  }, [])
 
   return (
     <div className="App">
       {props.error && <p className="error">{props.error}</p>}
-      <button onClick={fetchCards}>Fetch HS Cards</button>
-      <button onClick={testState}>Test State</button>
       <CardsDisplay />
       <PlayerHand />
     </div>
@@ -28,7 +21,6 @@ function App(props) {
 const mapStateToProps = state => ({
   cards: state.cards,
   error: state.error,
-  playerHand: state.playerHand,
 })
 
 export default connect(mapStateToProps, {fetchHSCards})(App);
--- src/components/CardsDisplay.js
+++ src/components/CardsDisplay.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux'
 import { addPlayerHand } from '../actions/index';
 
 const CardsDisplay = props => {
+  const [pageState, setPageState] = useState({
+    currPage: 1,
+    totalPages: 25
+  })
+
+  const { Classic } = props.cards;
+
+  let currPageCards = Classic.slice((pageState.currPage * 10 ) - 10, pageState.currPage * 10)
+
+  const onNextChange = () => {
+    setPageState(prevState => ({
+      ...prevState,
+      currPage: prevState.currPage + 1,
+      totalPages: Math.ceil(Classic.length / 10)
+    }))
+  }
+
+  const onPrevChange = () => {
+    setPageState(prevState => ({
+      ...prevState,
+      currPage: prevState.currPage - 1,
+      totalPages: Math.ceil(Classic.length / 10)
+    }))
+  }
+
   const addPlayerHand = card => {
     if (props.playerHand.length >= 10 || props.playerHand.includes(card)) {
       alert('Your hand is full or card already exists!')
@@ -12,16 +37,21 @@ const CardsDisplay = props => {
   }
 
   return (
-    <div className="card-image-container">
-      {props.cards.Classic.map(card => 
-        <img 
-          src={card.img} 
-          alt={card.name} 
-          key={card.cardId}
-          onClick={() => addPlayerHand(card)}
-          className="card-image"
-        />
-      )}
+    <div>
+      <div className="card-image-container">
+        {
+          currPageCards.map(card => 
+          <img 
+            src={card.img} 
+            alt={card.name} 
+            key={card.cardId}
+            onClick={() => addPlayerHand(card)}
+            className="card-image"
+          />)
+        }
+      </div>
+      <button onClick={onNextChange}> Next </button>
+      <button onClick={onPrevChange}> Prev </button>
     </div>
   )
 }
