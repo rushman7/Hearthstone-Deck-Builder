f4448ed871ecacd048b6a0e503e9b73de443ff21
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-10-13 00:10:37 -0400

    error message css

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -2,14 +2,27 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { css } from 'glamor';
 
 
 const ErrorMessage = props => {
-  const notify = () => toast(props.error);
+  const notify = () => toast(props.error ,{
+    className: css({
+      background: 'red'
+    }),
+    bodyClassName: css({
+      fontSize: '1rem',
+      color: 'white'
+    }),
+    progressClassName: css({
+      background: "#ffb7b7"
+    })
+  });
   return (
     <div>
       {props.error && <p>{notify()}</p>}
-      <ToastContainer />
+      <ToastContainer 
+        autoClose={8000}/>
     </div>
   );
 }
